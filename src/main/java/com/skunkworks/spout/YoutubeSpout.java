bb00ed52bb7b2c683be8a30e7116e6a90c24e0c0
Balchandra Dudka <balchandra.dudka@talentica.com>
2014-01-06 19:42:07 +0530

    Handled Java Null Pointer at 78

--- src/main/java/com/skunkworks/spout/YoutubeSpout.java
+++ src/main/java/com/skunkworks/spout/YoutubeSpout.java
@@ -75,8 +75,13 @@ public class YoutubeSpout extends BaseRichSpout{
 				 vids.setId(ids);
 				 List<Video> videos = vids.execute().getItems();
 				 for(Video video:videos){
-					 String weburl = video.getPlayer().getEmbedHtml().replace("<iframe type='text/html' src='", "");
+					 String origurl = video.getPlayer().getEmbedHtml();
+					 String weburl = null;
+					 if (origurl != null){
+							 
+					 weburl = origurl.replace("<iframe type='text/html' src='", "");
 					 weburl = weburl.replace("' width='640' height='360' frameborder='0' allowfullscreen='true'/>", "");
+					 }
 					 _collector.emit(new Values( "Cadbury" + "," + video.getId(), video.getSnippet().getTitle(), video.getStatistics().getViewCount().longValue()
 							 , video.getStatistics().getLikeCount().longValue(), video.getStatistics().getDislikeCount().longValue(),  
 							 video.getStatistics().getCommentCount().longValue(), video.getStatistics().getFavoriteCount().longValue(), weburl));
