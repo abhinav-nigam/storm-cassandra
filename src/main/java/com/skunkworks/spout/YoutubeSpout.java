e6c77a8cc32829bbdd7f5a845c03ab017deda8db
Balchandra Dudka <balchandra_dudka@culturemachine.in>
2014-01-06 18:38:18 +0530

    Added Title and WebUrl columns

--- src/main/java/com/skunkworks/spout/YoutubeSpout.java
+++ src/main/java/com/skunkworks/spout/YoutubeSpout.java
@@ -74,10 +74,15 @@ public class YoutubeSpout extends BaseRichSpout{
 				 ids = ids.replaceAll(" ,$", "");
 				 vids.setId(ids);
 				 List<Video> videos = vids.execute().getItems();
-				 for(Video video:videos)
-					 _collector.emit(new Values(video.getId() + "," + video.getSnippet().getTitle(), video.getStatistics().getViewCount().longValue()
+				 for(Video video:videos){
+					 String weburl = video.getPlayer().getEmbedHtml().replace("<iframe type='text/html' src='", "");
+					 weburl = weburl.replace("' width='640' height='360' frameborder='0' allowfullscreen='true'/>", "");
+					 _collector.emit(new Values( "Cadbury" + "," + video.getId(), video.getSnippet().getTitle(), video.getStatistics().getViewCount().longValue()
 							 , video.getStatistics().getLikeCount().longValue(), video.getStatistics().getDislikeCount().longValue(),  
-							 video.getStatistics().getCommentCount().longValue(), video.getStatistics().getFavoriteCount().longValue()));
+							 video.getStatistics().getCommentCount().longValue(), video.getStatistics().getFavoriteCount().longValue(), weburl));
+					 
+				 }
+					 
 			 }
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -86,7 +91,7 @@ public class YoutubeSpout extends BaseRichSpout{
 
 	@Override
 	public void declareOutputFields(OutputFieldsDeclarer declarer) {
-		declarer.declare(new Fields("key", "views", "likes", "dislikes", "comments", "favourites"));		
+		declarer.declare(new Fields("key", "title", "views", "likes", "dislikes", "comments", "favourites", "weburl"));		
 	}
 
 }
